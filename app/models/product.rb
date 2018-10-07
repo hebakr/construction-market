7986804b528c26ff7bacdf9b3e9b6facd410f352
Abubakr Mohamed <hebakr@gmail.com>
2018-10-07 22:48:40 +0200

    Code refactor

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,9 +1,9 @@
-# product model
 class Product < ApplicationRecord
   belongs_to :category
   has_many :photos
   has_many :attachements
   has_many :spec_values
   has_many :prices
-  has_and_belongs_to_many :providers, foreign_key: 'company_id', class_name: 'Company'
+  has_and_belongs_to_many :providers, foreign_key: 'company_id',
+                                      class_name: 'Company'
 end

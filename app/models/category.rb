bb5c5f2a5d5639d676ff6ee00cb331c83dc2d53a
Abubakr Mohamed <hebakr@gmail.com>
2018-10-28 18:50:24 +0200

    fix category relation

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,5 @@
 class Category < ApplicationRecord
-    belongs_to :parent, :class_name => 'Category'
+    belongs_to :category, :class_name => 'Category', optional: true
     has_many :sub_categories, :class_name => 'Category', :foreign_key => 'parent_id'
     has_many :brands
 

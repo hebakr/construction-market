84b33cb4a3b4792cddf6bc48a58fea7f9ce57614
Abubakr Mohamed <hebakr@gmail.com>
2018-11-14 00:00:45 +0200

    secure dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,11 @@
 class DashboardController < SecuredController
   layout false
+  before_action :authenticate_admin_user!
+
+  private
+
+  def authenticate_admin_user!
+    current_user.role?(:Admin)
+  end
 
 end
